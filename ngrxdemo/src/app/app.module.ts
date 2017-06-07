a82b1d264df28b9a64d2bf9a7bdd95148a581c9d
marcodi <marcodi@microsoft.com>
2017-06-06 22:01:49 -0400

    adding the reducer

--- ngrxdemo/src/app/app.module.ts
+++ ngrxdemo/src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { people } from './reducers/people';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    StoreModule.provideStore({ people }),
     BrowserModule
   ],
   providers: [],
