2b1ae083d9e335a08961df6c96d7ea605db2fc44
marcodi <marcodi@microsoft.com>
2017-06-06 22:18:31 -0400

    switch to constants for actions

--- ngrxdemo/src/app/reducers/people.ts
+++ ngrxdemo/src/app/reducers/people.ts
@@ -1,17 +1,25 @@
+import {
+    ADD_GUEST,
+    ADD_PERSON,
+    REMOVE_GUEST,
+    REMOVE_PERSON,
+    TOGGLE_ATTENDING
+    } from './../actions/actions';
+
 const details = (state, action) => {
     switch (action.type) {
-        case 'ADD_GUEST':
+        case ADD_GUEST:
             if (state.id === action.payload) {
                 return Object.assign({}, state, { guests: state.guests + 1 });
             }
             return state;
-        case 'REMOVE_GUEST':
+        case REMOVE_GUEST:
             if (state.id === action.payload) {
                 return Object.assign({}, state, { guests: state.guests - 1 });
             }
             return state;
 
-        case 'TOGGLE_ATTENDING':
+        case TOGGLE_ATTENDING:
             if (state.id === action.id) {
                 return Object.assign({}, state, { attending: !state.attending });
             }
@@ -24,23 +32,23 @@ const details = (state, action) => {
 // remember to avoid mutation within reducers
 export const people = (state = [], action) => {
     switch (action.type) {
-        case 'ADD_PERSON':
+        case ADD_PERSON:
             return [
                 ...state,
                 Object.assign({}, { id: action.payload.id, name: action.payload.name, guests: 0, attending: false })
             ];
 
-        case 'REMOVE_PERSON':
+        case REMOVE_PERSON:
             return state
                 .filter(person => person.id !== action.payload);
         // to shorten our case statements, delegate detail updates to second private reducer
-        case 'ADD_GUEST':
+        case ADD_GUEST:
             return state.map(person => details(person, action));
 
-        case 'REMOVE_GUEST':
+        case REMOVE_GUEST:
             return state.map(person => details(person, action));
 
-        case 'TOGGLE_ATTENDING':
+        case TOGGLE_ATTENDING:
             return state.map(person => details(person, action));
         // always have default return of previous state when action is not relevant
         default:
